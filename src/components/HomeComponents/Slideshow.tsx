1fb6122e1995cde0547af56601a2975bb0663568
user name <harshj6680@gmail.com>
2024-01-13 23:49:11 +0530

    slider and images

--- src/components/HomeComponents/Slideshow.tsx
+++ src/components/HomeComponents/Slideshow.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
-import Slider from 'react-slick';
-import image1 from '../../assets/24.jpg';
-import image2 from '../../assets/25.jpg';
-import image3 from '../../assets/1.jpg';
+import React from "react";
+import Slider from "react-slick";
+import wave from "../../assets/wave.svg";
 
 const Slideshow = () => {
   const settings = {
@@ -16,19 +14,24 @@ const Slideshow = () => {
   };
 
   return (
-    <div className="slideshow-container">
-      <Slider {...settings}>
-        <div>
-          <img src={image1} alt="Image 1" />
-        </div>
-        <div>
-          <img src={image2} alt="Image 2" />
-        </div>
-        <div>
-          <img src={image3} alt="Image 3" />
-        </div>
-      </Slider>
-    </div>
+    <>
+      <div className="wave">
+        <img src={wave} />
+      </div>
+      <div className="slideshow-container">
+        <Slider {...settings}>
+          <div>
+            <img src="/images/1.jpg" alt="Image 1" />
+          </div>
+          <div>
+            <img src="/images/24.jpg" alt="Image 2" />
+          </div>
+          <div>
+            <img src="/images/25.jpg" alt="Image 3" />
+          </div>
+        </Slider>
+      </div>
+    </>
   );
 };
 
