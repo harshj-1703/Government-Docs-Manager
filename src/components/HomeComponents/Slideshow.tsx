d9c543ef45462b94089bc5971bad6639fa7cfb84
user name <harshj6680@gmail.com>
2024-01-14 21:14:38 +0530

    slider updated

--- src/components/HomeComponents/Slideshow.tsx
+++ src/components/HomeComponents/Slideshow.tsx
@@ -3,46 +3,41 @@ import Slider from "react-slick";
 import wave from "../../assets/wave.svg";
 
 const Slideshow = () => {
-  const [isLoaded] = useState(false);
+  const [isLoaded, setIsLoaded] = useState([false, false, false]);
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2500,
+  const imageUrls = [
+    "https://www.india.gov.in/sites/upload_files/npi/files/spotlights/VanDhan.jpg",
+    "https://www.mygov.in/sites/all/themes/mygov/images/pmfby/pmfby-banner.jpg",
+    "https://www.india.gov.in/sites/upload_files/npi/files/spotlights/ujjwala-yojana-inner.jpg",
+    "https://static.theprint.in/wp-content/uploads/2018/08/Modi-Ujjawala.jpg",
+  ];
+
+  const handleImageLoad = (index: number) => {
+    const updatedIsLoaded = [...isLoaded];
+    updatedIsLoaded[index] = true;
+    setIsLoaded(updatedIsLoaded);
   };
 
   return (
     <>
       <div className="wave">
-        <img src={wave} />
+        <img src={wave} alt="Wave" />
       </div>
-      <div className="slideshow-container" style={{backgroundColor:"red"}}>
-        <Slider {...settings}>
-          <div>
-            {isLoaded ? (
-              <img src="/images/1.jpg" alt="Image 1" />
-            ) : (
-              <div className="skeleton-image"></div>
-            )}
-          </div>
-          <div>
-            {isLoaded ? (
-              <img src="/images/1.jpg" alt="Image 1" />
-            ) : (
-              <div className="skeleton-image"></div>
-            )}
-          </div>
-          <div>
-            {isLoaded ? (
-              <img src="/images/1.jpg" alt="Image 1" />
-            ) : (
-              <div className="skeleton-image"></div>
-            )}
-          </div>
+      <div className="slideshow-container" style={{ backgroundColor: "red" }}>
+        <Slider dots infinite autoplay autoplaySpeed={2500}>
+          {imageUrls.map((url, index) => (
+            <div key={index}>
+              {isLoaded[index] ? (
+                <img
+                  src={url}
+                  alt={`Image ${index + 1}`}
+                  onLoad={() => handleImageLoad(index)}
+                />
+              ) : (
+                <div className="skeleton-image"></div>
+              )}
+            </div>
+          ))}
         </Slider>
       </div>
     </>
