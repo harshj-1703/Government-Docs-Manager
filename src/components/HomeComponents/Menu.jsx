499c2198ef8a9dcd7eea2315de3aeb77ad78244d
user name <harshj6680@gmail.com>
2024-01-17 00:26:19 +0530

    updated colors

--- src/components/HomeComponents/Menu.jsx
+++ src/components/HomeComponents/Menu.jsx
@@ -1,23 +1,37 @@
+import IonIcon from "@reacticons/ionicons";
 import React from "react";
 
 function Menu() {
   return (
     <div className="home-menu">
       <ul className="home-menu-ul">
-        <li style={{ "--i": 6 }}>
-          <a href="#">Home</a>
+        <li style={{ "--i": 6, "--clr1": "#1877f2" }}>
+          <a href="#">
+            <span>H</span>
+            Home
+          </a>
         </li>
-        <li style={{ "--i": 5 }}>
-          <a href="#">User Login</a>
+        <li style={{ "--i": 5, "--clr1": "#25d366" }}>
+          <a href="#">
+            <span>U</span>User Login
+          </a>
         </li>
-        <li style={{ "--i": 4 }}>
-          <a href="#">User Registration</a>
+        <li style={{ "--i": 4, "--clr1": "#1da1f2" }}>
+          <a href="#">
+            <span>U</span>User Registration
+          </a>
         </li>
-        <li style={{ "--i": 3 }}>
-          <a href="#">Data Center Login</a>
+        <li style={{ "--i": 3, "--clr1": "#ff0000" }}>
+          <a href="#">
+            <span>D</span>
+            Data Center Login
+          </a>
         </li>
-        <li style={{ "--i": 2 }}>
-          <a href="#">Admin Login</a>
+        <li style={{ "--i": 2, "--clr1": "#0a66c2" }}>
+          <a href="#">
+            <span>A</span>
+            Admin Login
+          </a>
         </li>
       </ul>
     </div>
