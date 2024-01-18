3cde8a518923a168d2d5277825da2ba3cb6775ff
user name <harshj6680@gmail.com>
2024-01-18 23:37:30 +0530

    about section done

--- src/components/HomeComponents/AboutContent.jsx
+++ src/components/HomeComponents/AboutContent.jsx
@@ -1,24 +1,52 @@
 import React from "react";
-import { LazyLoadImage } from "react-lazy-load-image-component";
+import {
+  LazyLoadComponent,
+  LazyLoadImage,
+} from "react-lazy-load-image-component";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 function AboutContent() {
+  const controls = useAnimation();
+  const [ref, inView] = useInView({ triggerOnce: false });
+
+  React.useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
+  const fadeInUpVariant = {
+    hidden: { opacity: 0, y: 70 },
+    visible: { opacity: 1, y: 0 },
+  };
   return (
     <>
-      <div id="main-about">
-        <div id="box-photo-content">
-          <div>
-            <LazyLoadImage
-              id="hj-image"
-              src="/images/bhai.jpeg"
-              effect="blur"
-            />
+      <LazyLoadComponent>
+        <motion.div
+          initial="hidden"
+          ref={ref}
+          animate={controls}
+          variants={fadeInUpVariant}
+          transition={{ duration: 0.6, ease: "easeOut" }}
+        >
+          <div id="main-about">
+            <div id="box-photo-content">
+              <div>
+                <LazyLoadImage
+                  id="hj-image"
+                  src="/images/bhai.jpeg"
+                  effect="blur"
+                />
+              </div>
+            </div>
+            <div id="about-main-text">
+              <h2 data-text="Developed By">Developed By</h2>
+              <h1>Harsh Jolapara</h1>
+            </div>
           </div>
-        </div>
-        <div id="about-main-text">
-          <h2 data-text="Developed By">Developed By</h2>
-          <h1>Harsh Jolapara</h1>
-        </div>
-      </div>
+        </motion.div>
+      </LazyLoadComponent>
     </>
   );
 }
