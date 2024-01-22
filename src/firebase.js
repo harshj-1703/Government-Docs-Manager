aea2cbd0ca6ae7c73de9c5de088a522f908b2bca
user name <harshj6680@gmail.com>
2024-01-22 23:38:23 +0530

    env added

--- src/firebase.js
+++ src/firebase.js
@@ -4,17 +4,18 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAhpXnxVVfl81k073bPgbJLeSi_oNYq2Zw",
-  authDomain: "government-docs-fb805.firebaseapp.com",
-  projectId: "government-docs-fb805",
-  storageBucket: "government-docs-fb805.appspot.com",
-  messagingSenderId: "846010638795",
-  appId: "1:846010638795:web:53e6b8b22c46911c4d964e",
-  measurementId: "G-DT27JXH5XB"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const db= getFirestore(app);
-let auth = getAuth(app);
-export {db,auth};
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
