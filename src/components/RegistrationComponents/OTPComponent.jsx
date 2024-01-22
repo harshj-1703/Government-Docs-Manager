e2001dea6a34469ff11ae07b9ad46d22e666ad3a
user name <harshj6680@gmail.com>
2024-01-22 23:58:56 +0530

    otp timeout added

--- src/components/RegistrationComponents/OTPComponent.jsx
+++ src/components/RegistrationComponents/OTPComponent.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import ToastMessage from "../ToastMessage";
 import OTPInput from "react-otp-input";
 import { LazyLoadComponent } from "react-lazy-load-image-component";
 
 function OTPComponent({ otp, setOtp, verificationCode, setStep }) {
+  const [seconds, setSeconds] = useState(60);
+  const [timer, setTimer] = useState(null);
+
+  useEffect(() => {
+    setTimer(setInterval(() => setSeconds((prevSeconds) => prevSeconds - 1), 1000));
+    return () => clearInterval(timer);
+  }, []);
+
+  useEffect(() => {
+    if (seconds === 0) {
+      clearInterval(timer);
+      setStep(1);
+      ToastMessage({
+        message: "OTP timeout. Please request a new code.",
+        type: "error",
+      });
+    }
+  }, [seconds, timer, setStep]);
+
   const handleVerifyCode = () => {
     verificationCode.confirm(otp).then((result) => {
+      clearInterval(timer);
         setStep(3);
         ToastMessage({
           message: "Verification successful!",
@@ -22,6 +42,7 @@ function OTPComponent({ otp, setOtp, verificationCode, setStep }) {
     <>
       <LazyLoadComponent>
         <h2>Enter the verification code</h2>
+        <div className="time-out">Time left: {seconds} seconds</div>
         <div id="otp-input">
           <OTPInput
             value={otp}
