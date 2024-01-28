cd1acaa7526678a52a771d2da3180757831b5f61
user name <harshj6680@gmail.com>
2024-01-28 20:49:49 +0530

    blur added

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -1,15 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import UserNavbar from "../components/UserDashboard/UserNavbar";
 import "../css/userdashboard.css";
 import UserNavbarMenu from "../components/UserDashboard/UserNavbarMenu";
 
 function UserDashboard() {
   const [isMenuShow, setIsMenuShow] = useState(false);
+  const [gridData, setGridData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((response) => response.json())
+      .then((data) => setGridData(data.slice(0, 16)))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
   return (
     <div className="user-dashboard">
       {isMenuShow && <UserNavbarMenu />}
       <UserNavbar isMenuShow={isMenuShow} setIsMenuShow={setIsMenuShow} />
-      <div>UserDashboard</div>
+
+      {/* User Dashboard */}
+      <div
+        className={
+          isMenuShow
+            ? "user-dashboard-main-content-blur"
+            : "user-dashboard-main-content"
+        }
+      >
+        <div className="grid-container">
+          {gridData.map((item) => (
+            <div key={item.id} className="grid-item">
+              <img src={item.thumbnailUrl} alt={item.title} />
+              <p>{item.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
