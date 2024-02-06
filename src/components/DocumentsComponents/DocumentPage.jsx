9c526a3558e2127dbf4f11452f56c69f56ac0452
user name <harshj6680@gmail.com>
2024-02-06 22:24:49 +0530

    document page

--- src/components/DocumentsComponents/DocumentPage.jsx
+++ src/components/DocumentsComponents/DocumentPage.jsx
@@ -3,6 +3,24 @@ import "../../css/documentpage.css";
 import { useLocation, useNavigate } from "react-router-dom";
 import documentService from "../../services/document.services";
 import CircularLoading from "../CircularLoading";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
+const Skeleton = () => <div className="skeleton"></div>;
+
+function RenderSmoothImage({ src }) {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  return (
+    <div className="smooth-image-wrapper-grid">
+      {!imageLoaded && <Skeleton />}
+      <LazyLoadImage
+        src={src}
+        style={{ opacity: imageLoaded ? 1 : 0 }}
+        onLoad={() => setImageLoaded(true)}
+      />
+    </div>
+  );
+}
 
 function DocumentPage({ isMenuShow }) {
   const [isLoading, setIsLoading] = useState(true);
@@ -38,7 +56,20 @@ function DocumentPage({ isMenuShow }) {
               isMenuShow ? "document-detail-div-blur" : "document-detail-div"
             }
           >
-            {console.log(data)}
+            <div className="document-page-banner-image">
+              <RenderSmoothImage src={data.banner} />
+            </div>
+            <div className="document-page-title">{data.title}</div>
+            <div className="document-page-ministry">{data.ministry}</div>
+            <hr className="line" />
+            <div className="document-page-description">{data.description}</div>
+            <div className="document-page-state">
+              <div id="orange">Applicable&nbsp;</div>
+              <div id="white">in&nbsp;</div>
+              <div id="green">
+                {data.state == "india" ? "Whole India" : data.state}
+              </div>
+            </div>
           </div>
         </div>
       ) : (
