88f3a17e07a7abb4b34b10c7a9b034dce8b0be64
user name <harshj6680@gmail.com>
2024-02-25 14:16:44 +0530

    datacenter dashboard details done

--- src/components/DataCenterComponents/DataCenterDashboardGrid.jsx
+++ src/components/DataCenterComponents/DataCenterDashboardGrid.jsx
@@ -2,33 +2,43 @@ import React from "react";
 import "../../css/datacenter-dashboard-grid.css";
 
 function DataCenterDashboardGrid() {
-  const numberOfCards = 6;
-  const cards = [];
-  for (let i = 0; i < numberOfCards; i++) {
-    cards.push(
-      <div className="card-container" key={i}>
-        <div className="card">
-          <div className="front-content">
-            <p>Hover me</p>
-          </div>
-          <div className="content">
-            <p className="heading">Card Hover</p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipii voluptas ten
-              mollitia pariatur odit, ab minus ratione adipisci accusamus vel
-              est excepturi laboriosam magnam necessitatibus dignissimos
-              molestias.
-            </p>
-          </div>
-        </div>
-      </div>
-    );
-  }
+  const cards = [
+    {
+      heading: "Verify Users Documents",
+      content: "Check and verify user uploaded documents from your datacenter",
+      link: "verify-user-uploaded-docs",
+    },
+    {
+      heading: "Users Documents Status",
+      content: "Check status of user documents",
+    },
+    {
+      heading: "Person Details",
+      content: "Datacenter's head persons details",
+      link: "datacenter-persons",
+    },
+  ];
 
   return (
     <div className="datacenter-dashboard-grid">
       <h1>DataCenter Dashboard</h1>
-      <div className="all-cards">{cards}</div>
+      <div className="all-cards">
+        {cards.map((card, idx) => {
+          return (
+            <div className="card-container" key={idx}>
+              <div className="card">
+                <div className="front-content">
+                  <p>{card.heading}</p>
+                </div>
+                <div className="content">
+                  <p className="heading">{card.heading}</p>
+                  <p>{card.content}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
