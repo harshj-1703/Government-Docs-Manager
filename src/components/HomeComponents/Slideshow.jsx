675fc1cc621c850d51ad03f9ce6c41a600f8eb36
user name <harshj6680@gmail.com>
2024-03-25 23:14:39 +0530

    dynamic posters added

--- src/components/HomeComponents/Slideshow.jsx
+++ src/components/HomeComponents/Slideshow.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+import { db } from "../../firebase";
+import { collection, getDocs, query, orderBy, limit } from "firebase/firestore";
 
 const Skeleton = () => <div className="skeleton"></div>;
 
@@ -23,30 +25,58 @@ function RenderSmoothImage({ src }) {
 }
 
 const Slideshow = () => {
-  const imageArray = [
-    "https://t4.ftcdn.net/jpg/04/45/41/71/360_F_445417168_ySehXZbCO65QjGgjSdoYXfJO8ME8tSkk.jpg",
-    "https://img.freepik.com/free-vector/abstract-wavy-indian-flag-banner_1055-7052.jpg",
-    "https://www.shutterstock.com/image-vector/stain-brush-stroke-flag-india-260nw-1922760536.jpg",
-    "https://scgj.azadikaamritmahotsav.in/wp-content/uploads/2021/09/Web-Banner-Hindi_1600X500.jpg",
-  ];
+  const [posters, setPosters] = useState([]);
+
+  useEffect(() => {
+    const fetchPosters = async () => {
+      try {
+        const postersRef = collection(db, "Posters");
+        const latestPosters = query(
+          postersRef,
+          orderBy("createdAt", "desc"),
+          limit(1)
+        );
+        const querySnapshot = await getDocs(latestPosters);
+        const tempPosters = [];
+        if (!querySnapshot.empty) {
+          const posterData = querySnapshot.docs[0].data();
+          for (let i = 1; i <= 4; i++) {
+            tempPosters.push(posterData[`urlP${i}Photo`]);
+          }
+          setPosters(tempPosters);
+        } else {
+          console.log("No documents found");
+        }
+        for (i of posters) {
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchPosters();
+  }, []);
 
   return (
     <div className="slideshow-container">
-      <Carousel
-        infiniteLoop={true}
-        autoPlay={true}
-        stopOnHover={true}
-        showThumbs={false}
-        interval={2500}
-        dynamicHeight={false}
-        width="100%"
-      >
-        {imageArray.map((image, index) => (
-          <div key={index} className="carousel-item">
-            <RenderSmoothImage src={image} />
-          </div>
-        ))}
-      </Carousel>
+      {posters.length > 0 ? (
+        <Carousel
+          infiniteLoop={true}
+          autoPlay={true}
+          stopOnHover={true}
+          showThumbs={false}
+          interval={2500}
+          dynamicHeight={false}
+          width="100%"
+        >
+          {posters.map((image, index) => (
+            <div key={index} className="carousel-item">
+              <RenderSmoothImage src={image} />
+            </div>
+          ))}
+        </Carousel>
+      ) : (
+        <Skeleton />
+      )}
     </div>
   );
 };
